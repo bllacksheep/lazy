40b6984a8da18f6edf25e04bf7fc506fd9a51cc3
Iàsan Mac an Lighiche <7663736+bllacksheep@users.noreply.github.com>
2026-05-19 11:37:55 +0100

    python
    need pyenv

--- lazy.sh
+++ lazy.sh
@@ -37,3 +37,26 @@ echo "GitHub CLI version:" && gh --version
 echo "=== Script execution complete! ==="
 echo "Please run 'source ~/.bashrc' or restart your terminal to use Pulumi."
 
+echo "=== Installing PYENV ==="
+sudo apt install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl git \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+curl -fsSL https://pyenv.run | bash
+
+cat <<EOF >> ~/.profile
+export PYTHON_BUILD_CURL_OPTS="-k"
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+eval "$(pyenv virtualenv-init -)"
+EOF
+
+cat <<EOF >> ~/.bashrc
+export PYTHON_BUILD_CURL_OPTS="-k"
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+eval "$(pyenv virtualenv-init -)"
+EOF
+
+pyenv --version
